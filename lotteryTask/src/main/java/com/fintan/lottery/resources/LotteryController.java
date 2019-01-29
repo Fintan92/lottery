42f5006442693f3a2711f99437a50e849124f06c
finta <finta@LAPTOP-31G7GDDR>
2019-01-29 16:40:20 +0000

    Service, Ticket and Line class complete. Need to start on Controller.

--- lotteryTask/src/main/java/com/fintan/lottery/resources/LotteryController.java
+++ lotteryTask/src/main/java/com/fintan/lottery/resources/LotteryController.java
@@ -1,13 +1,20 @@
 package com.fintan.lottery.resources;
 
+import com.fintan.lottery.services.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value="/", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value="/" , produces = MediaType.APPLICATION_JSON_VALUE)
 public class LotteryController {
+	@Autowired
+	private LotteryService lotteryService;
 	
-	
+	@RequestMapping(value="/ticket", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+	public  createTicket()
 }
